90a49dd5bcd8e00330989c1f1377c6cab14f097c
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-06-02 12:37:07 -0600

    Ya le entendi

--- JS/operaciones.js
+++ JS/operaciones.js
@@ -1,1 +1,43 @@
-const { all } = require('mathjs')
+function igual(){
+    biseccion()
+}
+
+function biseccion() {
+    let expr = document.getElementById('texto').value;
+    let errorDeseado = document.getElementById('errorDeseado').value;
+    let a=1, b=2, c
+    let funcion = math.compile(expr)
+    let variableA, variableB, variableC
+    let i = 0, iteraciones = 10
+    let error
+    variableA = {x: a}
+    variableB = {x: b}
+    if(funcion.evaluate(variableA) * funcion.evaluate(variableB) < 0) {
+        do{
+            c = (a+b)/2
+            variableC = {x: c}          
+            if(funcion.evaluate(variableA) * funcion.evaluate(variableC) < 0){
+                b = c;
+            }
+            else {
+                if(funcion.evaluate(variableA) * funcion.evaluate(variableC) > 0){
+                    a = c
+                }
+                else {
+                    if(funcion.evaluate(variableA) * funcion.evaluate(variableC) == 0){
+                        error = 0
+                    }
+                }
+            }
+            error=Math.abs(funcion.evaluate(variableC) - 0)
+            i = i+1
+        }while(i < iteraciones || error > errorDeseado)
+        console.log("La raiz es: ",c)
+        console.log("El error es: ",error)
+        console.log("Numero de iteraciones: ",i)
+    }
+    else {
+        console.log("No hay raiz en este intervalo")
+    }
+}
+
