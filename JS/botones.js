83df7d9b4bd54e2b04344470b21f37e37f5c7ce6
Urielminpro705 <urielmata37@hotmail.com>
2023-06-04 16:01:36 -0600

    XD

--- JS/botones.js
+++ JS/botones.js
@@ -1,129 +1,127 @@
 function letraE() {
     var t = document.getElementById('texto');
-    t.value = t.value + "e"
+    t.value = t.value + "e";
 }
 
 function borrarUno() {
     var texto = document.getElementById('texto');
     var nuevoTexto = texto.value.slice(0, -1);
-    texto.value = nuevoTexto
+    texto.value = nuevoTexto;
+    limpiar();
 }
 
 function borrar() {
     var t = document.getElementById('texto');
-    t.value = null
-    let r = document.getElementById('resultado');
-    r.value = null
-    let r2 = document.getElementById('Nresultados');
-    r2.value = null
+    t.value = null;
+    limpiar();
 }
 
 function parentecis1() {
     var t = document.getElementById('texto');
-    t.value = t.value + "("
+    t.value = t.value + "(";
 }
 
 function parentecis2() {
     var t = document.getElementById('texto');
-    t.value = t.value + ")"
+    t.value = t.value + ")";
 }
 
 function raizCuadrada() {
     var t = document.getElementById('texto');
-    t.value = t.value + "sqrt("
+    t.value = t.value + "sqrt(";
 }
 
 function seno() {
     var t = document.getElementById('texto');
-    t.value = t.value + "sin("
+    t.value = t.value + "sin(";
 }
 
 function coseno() {
     var t = document.getElementById('texto');
-    t.value = t.value + "cos("
+    t.value = t.value + "cos(";
 }
 
 function division() {
     var t = document.getElementById('texto');
-    t.value = t.value + "/"
+    t.value = t.value + "/";
 }
 
 function multiplicar() {
     var t = document.getElementById('texto');
-    t.value = t.value + "*"
+    t.value = t.value + "*";
 }
 
 function siete() {
     var t = document.getElementById('texto');
-    t.value = t.value + "7"
+    t.value = t.value + "7";
 }
 
 function ocho() {
     var t = document.getElementById('texto');
-    t.value = t.value + "8"
+    t.value = t.value + "8";
 }
 
 function nueve() {
     var t = document.getElementById('texto');
-    t.value = t.value + "9"
+    t.value = t.value + "9";
 }
 
 function variable() {
     var t = document.getElementById('texto');
-    t.value = t.value + "x"
+    t.value = t.value + "x";
 }
 
 function cuatro() {
     var t = document.getElementById('texto');
-    t.value = t.value + "4"
+    t.value = t.value + "4";
 }
 
 function cinco() {
     var t = document.getElementById('texto');
-    t.value = t.value + "5"
+    t.value = t.value + "5";
 }
 
 function seis() {
     var t = document.getElementById('texto');
-    t.value = t.value + "6"
+    t.value = t.value + "6";
 }
 
 function mas() {
     var t = document.getElementById('texto');
-    t.value = t.value + "+"
+    t.value = t.value + "+";
 }
 
 function uno() {
     var t = document.getElementById('texto');
-    t.value = t.value + "1"
+    t.value = t.value + "1";
 }
 
 function dos() {
     var t = document.getElementById('texto');
-    t.value = t.value + "2"
+    t.value = t.value + "2";
 }
 
 function tres() {
     var t = document.getElementById('texto');
-    t.value = t.value + "3"
+    t.value = t.value + "3";
 }
 
 function menos() {
     var t = document.getElementById('texto');
-    t.value = t.value + "-"
+    t.value = t.value + "-";
 }
 
 function exponente() {
     var t = document.getElementById('texto');
-    t.value = t.value + "^"
+    t.value = t.value + "^";
 }
 
 function cero() {
     var t = document.getElementById('texto');
-    t.value = t.value + "0"
+    t.value = t.value + "0";
 }
 
 function punto() {
     var t = document.getElementById('texto');
-    t.value = t.value + "."
+    t.value = t.value + ".";
 }
--- JS/operaciones.js
+++ JS/operaciones.js
@@ -1,141 +1,144 @@
 function igual(){
     let expr = document.getElementById('texto').value;
-    let errorDeseado = 0
+    let errorDeseado = 0;
+    biseccion(expr, errorDeseado);
+}
+
+function limpiar() {
     let r = document.getElementById('resultado');
-    r.value = null
+    r.value = null;
     let r2 = document.getElementById('Nresultados');
-    r2.value = null
-    biseccion(expr, errorDeseado)
+    r2.value = null;
 }
 
 class Raiz{
     constructor (raiz, error, iteraciones){
-        this.raiz = raiz
-        this.error = error
-        this.iteraciones = iteraciones
+        this.raiz = raiz;
+        this.error = error;
+        this.iteraciones = iteraciones;
     }
 }
 
 function imprimir(raices) {
     var r = document.getElementById('resultado');
     var r2 = document.getElementById('Nresultados');
-    r2.value = "Se econtraron " + raices.length + " raices"
+    r2.value = "Se econtraron " + raices.length + " raices";
     for(var i = 0; i < raices.length; i++){
-        r.value = r.value + "La raíz es: " + raices[i].raiz + "\n"
-        console.log("La raiz es: ",raices[i].raiz)
-        console.log("El error es: ",raices[i].error)
-        console.log("Numero de iteraciones: ",raices[i].iteraciones,"\n")
+        r.value = r.value + "La raíz es: " + raices[i].raiz + "\n";
+        console.log("La raiz es: ",raices[i].raiz);
+        console.log("El error es: ",raices[i].error);
+        console.log("Numero de iteraciones: ",raices[i].iteraciones,"\n");
     }
 }
 
 function biseccion(expr, errorDeseado) {  
-    let funcion = math.compile(expr) 
-    var a=-50, b=-47, auxA, auxB
-    var raices = []
+    let funcion = math.compile(expr);
+    var a=-50, b=-47, auxA, auxB;
+    var raices = [];
     do{
-        var i = 0, error, c = 0
-        var variableA, variableB, variableC
-        let iteraciones = 100
-        variableA = {x: a}
-        variableB = {x: b}
-        auxA = a
-        auxB = b
+        var i = 0, error, c = 0;
+        var variableA, variableB, variableC;
+        let iteraciones = 100;
+        variableA = {x: a};
+        variableB = {x: b};
+        auxA = a;
+        auxB = b;
         if(funcion.evaluate(variableA) * funcion.evaluate(variableB) < 0) {
             do{                    
-                c = (a+b)/2                           
-                variableC = {x: c}      
+                c = (a+b)/2;                         
+                variableC = {x: c};      
                 if(funcion.evaluate(variableA) * funcion.evaluate(variableC) < 0){
                     b = c;
                 }
                 else {
                     if(funcion.evaluate(variableA) * funcion.evaluate(variableC) > 0){
-                        a = c
+                        a = c;
                     }
                     else {
                         if(funcion.evaluate(variableA) * funcion.evaluate(variableC) == 0){
-                            error = 0
+                            error = 0;
                         }
                     }
                 }
-                error = Math.abs(funcion.evaluate(variableC) - 0)
-                i = i+1
+                error = Math.abs(funcion.evaluate(variableC) - 0);
+                i = i+1;
             }while(i < iteraciones && error > errorDeseado)
-            a = auxA
-            b = auxB
-            var raiz = new Raiz(c, error, i)
-            raices.push(raiz)
-            imprimir(raiz)
-            a = a+3
-            b = b+3
+            a = auxA;
+            b = auxB;
+            var raiz = new Raiz(c, error, i);
+            raices.push(raiz);
+            imprimir(raiz);
+            a = a+3;
+            b = b+3;
         }
         else {
-            a = a+3
-            b = b+3
+            a = a+3;
+            b = b+3;
         }
-    }while(b < 60)
-    imprimir(raices)
+    }while(b < 60);
+    imprimir(raices);
 }
 
 function newton(expr, errorDeseado){
-    let a = 1 , iteraciones = 100, b, error, i = 0
-    let funcion = math.compile(expr)
-    var variableA, derivada
-    derivada = math.derivative(expr,'x').toString()
-    let derivada1 = math.compile(derivada) 
+    let a = 1 , iteraciones = 100, b, error, i = 0;
+    let funcion = math.compile(expr);
+    var variableA, derivada;
+    derivada = math.derivative(expr,'x').toString();
+    let derivada1 = math.compile(derivada);
     do{
-        variableA = {x: a}
-        b = a - (funcion.evaluate(variableA)/derivada1.evaluate(variableA))
-        a = b
-        error = Math.abs(funcion.evaluate(variableA) - 0)
-        i++
-    }while(i < iteraciones && error > errorDeseado)
-    console.log("La derivada es: ",derivada)
-    console.log("La raiz es: ", b)
-    console.log("El error es: ",error)
-    console.log("Numero de iteraciones: ",i)
+        variableA = {x: a};
+        b = a - (funcion.evaluate(variableA)/derivada1.evaluate(variableA));
+        a = b;
+        error = Math.abs(funcion.evaluate(variableA) - 0);
+        i++;
+    }while(i < iteraciones && error > errorDeseado);
+    console.log("La derivada es: ",derivada);
+    console.log("La raiz es: ", b);
+    console.log("El error es: ",error);
+    console.log("Numero de iteraciones: ",i);
 }
 
 function puntoFijo(expr, errorDeseado) {
-    let b, error
-    var i = 0, a = 0
-    let iteraciones = 100
-    let funcion = math.compile(expr)
-    let variableA
+    let b, error;
+    var i = 0, a = 0;
+    let iteraciones = 100;
+    let funcion = math.compile(expr);
+    let variableA;
     do {
-        variableA = {x: a}
-        b = funcion.evaluate(variableA)
-        error = Math.abs(b - a)
-        a = b
-        i++
-    }while(i < iteraciones && error > errorDeseado)
+        variableA = {x: a};
+        b = funcion.evaluate(variableA);
+        error = Math.abs(b - a);
+        a = b;
+        i++;
+    }while(i < iteraciones && error > errorDeseado);
     if(i <= iteraciones) {
-        console.log("La raiz es: ", a)
-        console.log("El error es: ", error)
-        console.log("Numero de iteraciones: ", i)
+        console.log("La raiz es: ", a);
+        console.log("El error es: ", error);
+        console.log("Numero de iteraciones: ", i);
     }
     else{
-        console.log("El metodo no converge en ",i," iteraciones")
+        console.log("El metodo no converge en ",i," iteraciones");
     } 
 }
 
 function secante(expr, errorDeseado) {
-    let iteraciones = 100
-    let funcion = math.compile(expr)
-    var a = 1, c, error, i = 0
-    var b = a - 0.00001
-    let variableA, variableB
+    let iteraciones = 100;
+    let funcion = math.compile(expr);
+    var a = 1, c, error, i = 0;
+    var b = a - 0.00001;
+    let variableA, variableB;
     do{
-        variableA = {x: a}
-        variableB = {x: b}
-        c = a - (((a - b)*funcion.evaluate(variableA))/(funcion.evaluate(variableA) - funcion.evaluate(variableB)))
-        b = a
-        a = c
-        error = Math.abs(funcion.evaluate(variableA) - 0)
-        i++
-    }while(i < iteraciones && error > errorDeseado)
-    console.log("La raiz es: ", c)
-    console.log("El error es: ", error)
-    console.log("Numero de iteraciones: ", i)
+        variableA = {x: a};
+        variableB = {x: b};
+        c = a - (((a - b)*funcion.evaluate(variableA))/(funcion.evaluate(variableA) - funcion.evaluate(variableB)));
+        b = a;
+        a = c;
+        error = Math.abs(funcion.evaluate(variableA) - 0);
+        i++;
+    }while(i < iteraciones && error > errorDeseado);
+    console.log("La raiz es: ", c);
+    console.log("El error es: ", error);
+    console.log("Numero de iteraciones: ", i);
 }
 /*Funciones de prueba:
     2x^2-x-5
